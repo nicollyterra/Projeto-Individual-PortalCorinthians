55a7bbf2653c873d4360c246f85754af9f83994f
nicollyterra <nicolly.terra@sptech.school>
2025-04-28 22:15:38 -0300

    carrossel finalmente deu certo obg jesus

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.news-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
