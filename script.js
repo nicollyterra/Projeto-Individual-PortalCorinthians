ac0846f0abff15cf57c3e30b3e67798f761ac474
nicollyterra <nicolly.terra@sptech.school>
2025-05-07 15:42:11 -0300

    começando modal de enquete

--- script.js
+++ script.js
@@ -50,3 +50,29 @@ toggleButton.addEventListener('click', () => {
     mainContent.classList.toggle('sidebar-open');
 });
 
+// código modal 
+
+// Criando uma função arrow chamada switchModal, 
+// que fará o seguinte processo quando executada:
+
+const switchModal = () => { //não usa o this próprio
+    const modal = document.querySelector('.modal') // armazenando o modal em uma constante usando o querySelector e selecionando elemento com a classe modal 
+    const actualStyle = modal.style.display // pegando o estilo atual em outra constante usando o do modal já armazenado 
+
+    if (actualStyle == 'block') {
+        modal.style.display = 'none' 
+    } 
+    else {
+        modal.style.display = 'block' // aqui o modal aparece
+    }
+}
+
+    const btn_craque = document.querySelector('.btn_craque')
+    btn_craque.addEventListener('click', switchModal) //diz que quando o botão for clicado chama a arrow function de switch modal 
+
+    window.onclick = function (event) {
+        const modal = document.querySelector ('.modal')
+    if (event.target == modal) {
+        switchModal()
+    }
+    }
